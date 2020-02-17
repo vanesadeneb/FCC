0c9d02803ea7ae84526a9721be6216e8a309314c
Vanesa Villarreal <vanesadeneb@users.noreply.github.com>
2020-02-16 23:00:33 -0700

    Create recordCollection.js
    Practice from FCC

---
+++ jsPractice/recordCollection.js
@@ -0,0 +1,47 @@
+var collection = {
+  2548: {
+    album: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: [
+      "Let It Rock",
+      "You Give Love a Bad Name"
+    ]
+  },
+  2468: {
+    album: "1999",
+    artist: "Prince",
+    tracks: [
+      "1999",
+      "Little Red Corvette"
+    ]
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [ ]
+  },
+  5439: {
+    album: "ABBA Gold"
+  }
+};
+
+// Only change code below this line
+function updateRecords(id, prop, value) {
+  
+  if(prop != "tracks" && value != ""){
+    collection[id][prop] = value
+  }
+  if(prop == "tracks" && !collection[id][prop]){
+    var array = [];
+    collection[id][prop] = array;
+  }
+  if(prop == "tracks" && value != ""){
+    collection[id][prop].push(value);
+  }
+  if(value == ""){
+    delete collection[id][prop]; 
+  }
+  return collection;
+}
+
+// Alter values below to test your code
+updateRecords(5439, "artist", "ABBA");
