9586d9b3921ce798a564ed49553590d6bc2be1d3
Vanesa Villarreal <vanesadeneb@users.noreply.github.com>
2020-03-01 12:00:34 -0700

    Regex tag i
    Finding more than the first match

---
+++ jsPractice/Regex/find-more-than-the-first-match.js
@@ -0,0 +1,3 @@
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/ig; // To search or extract a pattern more than once use the g tag.
+let result = twinkleStar.match(starRegex); 
