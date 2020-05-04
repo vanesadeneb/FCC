73f5b559495011d55bb1f4bba296329bde47447f
Vanesa Villarreal <vanessa_diesver@hotmail.com>
2020-05-04 13:26:48 -0700

    generating an array of all object keys with object

---
+++ dataStructure/generate-an-array-of-all-object-keys-with-object.keys.js
@@ -0,0 +1,25 @@
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  //Generate an array which contains all the keys stored in an object
+  return Object.keys(obj);
+}
+
+console.log(getArrayOfUsers(users));
