4d1cf7f5dc4aeb3c149fb7b366b75ae6b0e4f177
Vanesa Villarreal <vanessa_diesver@hotmail.com>
2020-05-07 19:59:32 -0700

    Using a Constructor to create Objects

---
+++ jsPractice/ObjectOrientedProgramming/define-a-constructor-function.js
@@ -0,0 +1,6 @@
+//Constructors are functions that create new objects. They define properties and behaviors that will belong to the new object. Think of them as a blueprint for the creation of new objects.
+function Dog(){
+  this.name = "Brandy";
+  this.color = "brown";
+  this.numLegs = 4;
+}
