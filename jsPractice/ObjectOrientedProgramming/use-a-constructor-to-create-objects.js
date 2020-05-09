7d97866999761c5db8287d9f186c95d3cbed03a0
Vanesa Villarreal <vanessa_diesver@hotmail.com>
2020-05-09 12:15:17 -0700

    using a contructor to create objects

---
+++ jsPractice/ObjectOrientedProgramming/use-a-constructor-to-create-objects.js
@@ -0,0 +1,7 @@
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Notice that the new operator is used when calling a constructor. This tells JavaScript to create a new instance.
+let hound = new Dog();
