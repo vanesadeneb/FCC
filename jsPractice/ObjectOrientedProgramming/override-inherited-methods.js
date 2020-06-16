efde6a3ae4964facce22ca467361c678e97b6410
Vanesa Villarreal <vanesadeneb@users.noreply.github.com>
2020-06-16 12:21:24 -0700

    overriding inherited methods

---
+++ jsPractice/ObjectOrientedProgramming/override-inherited-methods.js
@@ -0,0 +1,14 @@
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Override the fly() method for Penguin so that it returns "Alas, this is a flightless bird."
+Penguin.prototype.fly = function(){ return "Alas, this is a flightless bird."}
+
+
+let penguin = new Penguin();
+console.log(penguin.fly());
